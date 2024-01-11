ba55b44f881b01c9bede581ee8bf30dfd5868b46
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2024-01-11 12:04:15 -0300

    otimizacao do codigo

--- src/script.js
+++ src/script.js
@@ -1,10 +1,16 @@
 const nao = document.getElementById("nao")
+const sim = document.getElementById("sim")
 var position = 0;
 var isAnimating = false;
+// isAnimating é usada para garantir que a animação no botão "Não" 
+// não seja acionada novamente se já estiver em andamento. Isso evita 
+// comportamentos inesperados e garante que a animação atual seja concluída 
+// antes que uma nova seja iniciada.
 
+nao.addEventListener("mouseover", animateNao);
+nao.addEventListener("click", animateNao);
 
-
-nao.addEventListener("mouseover", function() {
+function animateNao(){
     if (!isAnimating) {
         isAnimating = true;
         position = position === 0 ? 150 : 0;
@@ -15,24 +21,7 @@ nao.addEventListener("mouseover", function() {
             isAnimating = false;
         }, 200);
     }
-
-});
-
-nao.addEventListener("click", function() {
-    if (!isAnimating) {
-        isAnimating = true;
-        position = position === 0 ? 150 : 0;
-        nao.style.transform = `translate(0px, ${position}px)`;
-        nao.style.transition = "all 0.2s ease";
-
-        setTimeout(function() {
-            isAnimating = false;
-        }, 200);
-    }
-
-});
-
-const sim = document.getElementById("sim")
+}
 
 sim.addEventListener("click", function(){
     window.location.href='https://www.youtube.com/shorts/aueTWlsmAKE'
